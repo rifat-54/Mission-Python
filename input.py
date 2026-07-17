506b88e46f08a962576174a572309dc7b3f24df5
rifat <rifat999por@gmail.com>
2026-07-17 20:45:57 +0600

    explore input function in python

---
+++ input.py
@@ -0,0 +1,11 @@
+name= input("enter your name :")
+print("welcome",name)
+
+
+val=int(input("enter value:"))
+
+age=float(input("enter age:"))
+
+
+print(type(val))
+print(type(age))
---
+++ practise1.py
@@ -0,0 +1,22 @@
+# a=int(input("enter first number:"))
+# b=int(input("enter second nubmer"))
+
+# sum=a+b
+# print("sum of two value: ",sum)
+
+
+# side=float(input("enter square side:"))
+
+# print("area=",side**2)
+
+# a=float(input("enter fist float number:"))
+# b=float(input("enter second float numbre:"))
+
+# avg=(a+b)/2
+
+# print(avg)
+
+a=int(input("enter fist value:"))
+b=int(input("enter second value:"))
+
+print(a>=b)
