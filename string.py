18d6cccc10f7c321df0f9ada89d40fc308bfcd3e
rifat <rifat999por@gmail.com>
2026-07-20 01:58:02 +0600

    explore string function|

--- string.py
+++ string.py
@@ -14,6 +14,21 @@
 
 # print(len(str3))
 
-str1="my name is rifat"
+# str1="my name is rifat"
 
-print(str1[3])
+# print(str1[3])
+
+
+str1="apple name"
+
+# print(str1[-2:-5])
+
+# print(str1.endswith("m"))
+
+# print(str1.capitalize())
+
+# print(str1.replace("name","fruit"))
+
+# print(str1.find("name"))
+
+print(str1.count("a"))
