ff49d6f1fb62e7ae99c773ff6904af21bb0ab8d0
rifat <rifat999por@gmail.com>
2026-07-21 02:40:09 +0600

    explore condition

---
+++ condition.py
@@ -0,0 +1,33 @@
+# light="blue"
+
+# if(light=="green"):
+#     print("go")
+# elif(light=="red"):
+#     print("stop")
+# elif(light=="yellow"):
+#     print("look")
+# else:
+#     print("ligh is broken")
+
+# marks=int(input("enter your marks:"))
+
+# if(marks>=90):
+#     grade="A"
+# elif(marks>=80 and marks<90):
+#     grade="B"
+# elif(marks>=70 and marks<80):
+#     grade="c"
+# else:
+#     grade="D"
+
+
+# print("your grade is : ",grade)
+
+
+age=int(input("enter your age: "))
+
+if(age>=18):
+    if(age>60):
+        print("Cannot drive")
+    else:
+        print("Can drive")
