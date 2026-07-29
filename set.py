cb3ee3ca86e397c8bc7432475e87de22ccf0f922
rifat <rifat999por@gmail.com>
2026-07-30 01:48:32 +0600

    explore set method

--- set.py
+++ set.py
@@ -1,5 +1,27 @@
-collection ={1,3,5,6,3,4,"hello","world","world"}
+# collection ={1,3,5,6,3,4,"hello","world","world"}
 
-print(collection)
-print(type(collection))
-print(len(collection))
+# collection=set()
+
+# collection.add(1)
+# collection.add(2)
+# collection.add(2)
+
+
+# collection.add((1,5,7,2))
+# collection.add("rifat")
+
+# collection.remove(2)
+
+# collection.clear()
+
+# print(collection.pop())
+# print(type(collection))
+# print(len(collection))
+
+
+set1={1,2,3}
+set2={3,4,5}
+
+print(set1.union(set2))
+
+print(set2.intersection(set1))
---
+++ setProblem.py
@@ -0,0 +1,38 @@
+# dist={
+#     "table":["a piece of furnicher","list of fac ts & figures"],
+#     "cat":"a small animal"
+# }
+
+# print(dist)
+
+# sub=set()
+
+# sub.add("python")
+# sub.add("java")
+# sub.add("c++")
+# sub.add("python")
+# sub.add("javascript")
+# sub.add("java")
+# sub.add("python")
+# sub.add("java")
+# sub.add("c++")
+# sub.add("c")
+
+# print(len(sub))
+
+subject={}
+
+subject["math"]=34
+subject["eng"]=6
+subject["bio"]=47
+
+print(subject)
+
+# st={9,"9.0"}
+
+st={
+    ("int","9"),
+    ("flot","9.0")
+}
+
+print(st)
