8d7e9cc1e95a8fd1f85a069e19cb920e1b891240
rifat <rifat999por@gmail.com>
2026-08-01 02:20:39 +0600

    explore for loop break and continue

--- loop.py
+++ loop.py
@@ -1,5 +1,62 @@
-count =1
+# count =1
 
-while count<=5:
-    print("hello")
-    count+=1
+# while count<=5:
+#     print("hello")
+#     count+=1
+
+# i=1
+
+# n=2
+
+# search=25
+
+# while i<=10:
+#     print(i*i)
+#     if(i*i==search):
+#         print("found ->",i-1)
+#     i+=1
+
+# print("loop ended")
+
+# list1=[]
+
+
+# i=1
+ 
+
+# while i<=10:
+# #   print(i)
+#   list1.append(i*i)
+#   i+=1
+
+# print(list1)
+# j=0
+
+# while j<len(list1):
+#   print(list1[j])
+#   j+=1
+
+
+# i=0
+
+# while i<=5:
+#     i+=1
+#     print(i)
+#     if(i==3):
+#         # break
+#         continue
+#     print("hi")
+
+
+#  print odd nm
+
+i=1
+
+while i<=10:
+    if(i%2!=0):
+        i+=1
+        continue
+    print(i)
+    i+=1
+
+
