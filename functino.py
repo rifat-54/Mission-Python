47b30363243c37a7c02e6ef4e72342f84d412d6a
rifat <rifat999por@gmail.com>
2026-08-07 23:02:40 +0600

    explore functin in python

---
+++ functino.py
@@ -0,0 +1,27 @@
+def add(a,b):
+    sum=a+b
+    print(sum)
+
+
+# add(2,4)
+# add(5,4)
+
+
+# def calmultipy(a,b):
+#     return a*b
+
+# multiply=calmultipy(23,76)
+
+# print(multiply)
+
+# def printHello(name):
+#     print("hello",name)
+
+
+# printHello("Rifat")
+
+def avarage3num(a,b,c):
+    avg=(a+b+c)/3
+    print(avg)
+
+avarage3num(6,9,11)
