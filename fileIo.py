e0627f7e471bacf1da0e94a132558f37ed445c19
rifat <rifat999por@gmail.com>
2026-08-15 11:58:59 +0600

    explore import and delete file in python

--- fileIo.py
+++ fileIo.py
@@ -1,4 +1,4 @@
-f=open("demo.txt","r")
+# f=open("demo.txt","r")
 
 # print(f)
 # print(type(f))
@@ -8,10 +8,32 @@ f=open("demo.txt","r")
 
 # print(type(data))
 
-line1=f.readline()
-print(line1)
+# line1=f.readline()
+# print(line1)
 
-line2=f.readline()
-print(line2)
+# line2=f.readline()
+# print(line2)
 
-f.close()
+# f=open("demo.txt","a")
+
+# f.write("\nwhere you go")
+
+# f=open("sample.txt","w")
+
+# f.close()
+
+# f=open("demo.txt","r+")
+# f=open("demo.txt","w+")
+# f=open("demo.txt","a+")
+
+# print(f.read())
+# f.write("z")
+
+# f.close()
+
+with open("demo.txt","r") as f:
+    data=f.read()
+    print(data)
+
+with open("demo.txt","w") as f:
+    f.write("new data")
